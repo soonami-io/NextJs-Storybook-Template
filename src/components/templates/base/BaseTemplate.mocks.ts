b8ddce774713c9199c9a87b0649320348b64a2dc
David Mood <davidmood@hotmail.com>
2023-02-20 19:25:57 +0330

    feat: add BaseTemplate component

---
+++ src/components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,14 @@
+import { IBaseTemplate } from './BaseTemplate';
+
+const base: IBaseTemplate = {
+  sampleTextProp: 'Hello world!',
+};
+
+const base_alt: IBaseTemplate = {
+  sampleTextProp: 'Bonjour!',
+};
+
+export const mockBaseTemplateProps = {
+  base,
+  base_alt,
+};
